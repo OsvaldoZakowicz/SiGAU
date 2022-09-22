9b63144563461b0054237e4b3516f294095abfe4
Osvaldo Zakowicz <osvaldozakowicz@gmail.com>
2022-09-22 10:53:53 -0300

    Modificado: rutas web con ruta a nueva vista estudiantes, vistas app, navegacion y dashboard para ajustar header, nav e iniciar panel admin

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -1,24 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-l text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Students') }}
         </h2>
     </x-slot>
 
-    <!-- vista dashboard de administracion -->
-		<div class="flex flex-row">
-			<aside class="flex-none w-24 bg-white border-r-2 border-gray-200">
-				<p>aside</p>
-			</aside>
-			<div class="flex-initial bg-white border-gray-200">
-				<p>main</p>
-			</div>
-		</div>
+		<!-- vista de estudiantes -->
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    Welcome student, you're logged in!
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/student', function () {
+    return view('student');
+})->middleware(['auth'])->name('student');
+
 require __DIR__.'/auth.php';
