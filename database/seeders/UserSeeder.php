86e92ea939637620df5245faa2f8728bb4c6dc35
Osvaldo Zakowicz <osvaldozakowicz@gmail.com>
2022-10-04 23:09:42 -0300

    Modificado: user seeder con cuentas de prueba

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Seeder;
 
 //modelo User
 use App\Models\User;
+//modelo Role
+use Spatie\Permission\Models\Role;
+//modelo Permission
+use Spatie\Permission\Models\Permission;
 
 class UserSeeder extends Seeder
 {
@@ -17,13 +21,69 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //*crear un administrador por defecto
-        //NOTA: debe tener email valido
+        //*crear un super arministrador
         User::create([
             'name' => 'super admin',
             'email' => 'admin@admin.com',
             'email_verified_at' => now(),
-            'password' => bcrypt('12345678')
+            'password' => bcrypt('password')
         ]);
+
+        //administrador base
+        User::create([
+            'name' => 'jose',
+            'email' => 'jose@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password')
+        ])->assignRole('administrador');
+
+        //auditor
+        User::create([
+            'name' => 'maria',
+            'email' => 'maria@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password')
+        ])->assignRole('auditor');
+
+        //encargado de albergues
+        User::create([
+            'name' => 'pepe',
+            'email' => 'pepe@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password')
+        ])->assignRole('encargado albergues');
+
+        //secretario general
+        User::create([
+            'name' => 'miranda',
+            'email' => 'miranda@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password')
+        ])->assignRole('secretario general');
+
+        //estudiante
+        User::create([
+            'name' => 'carlos',
+            'email' => 'carlos@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password')
+        ])->assignRole('estudiante');
+
+        //becado
+        User::create([
+            'name' => 'laura',
+            'email' => 'laura@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password')
+        ])->assignRole('becado');
+
+        //delegado
+        User::create([
+            'name' => 'osvaldo',
+            'email' => 'osvaldo@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password')
+        ])->assignRole('delegado');
+
     }
 }
