1bc5ea2ef5be54800fe0c6d968dda728ffd224ab
Osvaldo Zakowicz <osvaldozakowicz@gmail.com>
2023-01-09 10:42:43 -0300

    Modificado: cabecera de la vista index y show de auditoria con mejor estilo de encabezado

--- resources/views/audits/index.blade.php
+++ resources/views/audits/index.blade.php
@@ -1,138 +1,143 @@
 @extends('dashboard')
 @section('dashboard-content')
     <div class="w-full h-max flex flex-col p-1">
-        <div class="mx-2 flex flex-col items-start justify-start">
-            <h3 class="text-base text-zinc-800 capitalize">auditoria: listado de los últimos movimientos</h3>
+        {{-- encabezado --}}
+        <div class="bg-zinc-300">
+            {{-- titulo de seccion --}}
+            <div class="p-1 flex justify-center items-center bg-zinc-400">
+                <h3 class="text-sm font-bold text-zinc-800 uppercase inline-block">auditoria: listado de los últimos movimientos</h3>
+            </div>
             {{-- menu de recargar, filtros, busqueda, reporte --}}
-            <div class="w-full flex items-end justify-between">
-                <x-buttons.button-link-zinc-light href="{{ route('audits.index') }}" class="mr-1" title="refrescar lista">
-                    <i class="fa-solid fa-rotate text-zinc-600 mr-1"></i>
-                    <span>recargar</span>
-                </x-buttons.button-link-zinc-light>
-                {{-- formulario de busqueda --}}
-                <div class="w-full">
-                    <form action="{{route('audits.index')}}" method="GET">
-                        <div class="flex items-end">
-                            {{-- evento --}}
-                            <div class="flex flex-col items-start">
-                                <label for="" class="text-sm">Evento</label>
-                                <select name="event"
-                                    class="w-36 p-0.5 mr-1 rounded-md shadow-sm border-zinc-300 focus:border-zinc-300 focus:ring focus:ring-zinc-200 focus:ring-opacity-50 text-sm">
-                                    <option value="all">todos</option>
-                                    <option value="created">creado</option>
-                                    <option value="updated">actualizado</option>
-                                    <option value="deleted">eliminado</option>
-                                    <option value="restored">restaurado</option>
-                                </select>
+            <div class="m-1">
+                <div class="w-full flex items-end justify-between">
+                    <x-buttons.button-link-zinc-light href="{{ route('audits.index') }}" class="mr-1" title="refrescar lista">
+                        <i class="fa-solid fa-rotate text-zinc-600 mr-1"></i>
+                        <span>recargar</span>
+                    </x-buttons.button-link-zinc-light>
+                    {{-- formulario de busqueda --}}
+                    <div class="w-full">
+                        <form action="{{route('audits.index')}}" method="GET">
+                            <div class="flex items-end">
+                                {{-- evento --}}
+                                <div class="flex flex-col items-start">
+                                    <label for="" class="text-sm">Evento</label>
+                                    <select name="event"
+                                        class="w-36 p-0.5 mr-1 rounded-md shadow-sm border-zinc-300 focus:border-zinc-300 focus:ring focus:ring-zinc-200 focus:ring-opacity-50 text-sm">
+                                        <option value="all">todos</option>
+                                        <option value="created">creado</option>
+                                        <option value="updated">actualizado</option>
+                                        <option value="deleted">eliminado</option>
+                                        <option value="restored">restaurado</option>
+                                    </select>
+                                </div>
+                                {{-- tabla --}}
+                                <div class="flex flex-col items-start">
+                                    <label for="" class="text-sm">Tabla</label>
+                                    <select name="table"
+                                        class="w-36 p-0.5 mr-1 rounded-md shadow-sm border-zinc-300 focus:border-zinc-300 focus:ring focus:ring-zinc-200 focus:ring-opacity-50 text-sm">
+                                        <option value="all">todas</option>
+                                        @foreach ($tables as $key => $value)
+                                            <option value="{{$key}}">{{__($value)}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                {{-- responsable --}}
+                                <div class="flex flex-col items-start">
+                                    <label for="" class="text-sm">Responsable</label>
+                                    <select name="responsible"
+                                        class="w-36 p-0.5 mr-1 rounded-md shadow-sm border-zinc-300 focus:border-zinc-300 focus:ring focus:ring-zinc-200 focus:ring-opacity-50 text-sm">
+                                        <option value="all">todos</option>
+                                        @foreach ($roles as $key => $value)
+                                            <option value="{{$key}}">{{$value}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                {{-- busqueda --}}
+                                <div class="flex flex-col items-start w-full mr-1">
+                                    <label for="" class="text-sm">Buscar por id de registro</label>
+                                    @error('search')
+                                        <span class="text-xs text-red-600">{{ $message }}</span>
+                                    @enderror
+                                    <input type="text" name="search" placeholder="buscar ..."
+                                        class="p-0.5 w-full rounded-md shadow-sm border-zinc-300 focus:border-zinc-300 focus:ring focus:ring-zinc-200 focus:ring-opacity-50 text-sm">
+                                </div>
+                                {{-- orden de listado --}}
+                                <div class="flex flex-col items-start mr-1">
+                                    <label for="" class="text-sm">Orden</label>
+                                    <select name="order"
+                                        class="w-36 p-0.5 rounded-md shadow-sm border-zinc-300 focus:border-zinc-300 focus:ring focus:ring-zinc-200 focus:ring-opacity-50 text-sm">
+                                        <option value="asc">a&rarr;z</option>
+                                        <option value="desc">z&rarr;a</option>
+                                    </select>
+                                </div>
+                                {{-- submit --}}
+                                <x-buttons.button-submit-green class="ml-1">
+                                    <i class="fa-solid fa-magnifying-glass mr-1"></i>
+                                    <span>buscar</span>
+                                </x-buttons.button-submit-green>
                             </div>
-                            {{-- tabla --}}
-                            <div class="flex flex-col items-start">
-                                <label for="" class="text-sm">Tabla</label>
-                                <select name="table"
-                                    class="w-36 p-0.5 mr-1 rounded-md shadow-sm border-zinc-300 focus:border-zinc-300 focus:ring focus:ring-zinc-200 focus:ring-opacity-50 text-sm">
-                                    <option value="all">todas</option>
-                                    @foreach ($tables as $key => $value)
-                                        <option value="{{$key}}">{{__($value)}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            {{-- responsable --}}
-                            <div class="flex flex-col items-start">
-                                <label for="" class="text-sm">Responsable</label>
-                                <select name="responsible"
-                                    class="w-36 p-0.5 mr-1 rounded-md shadow-sm border-zinc-300 focus:border-zinc-300 focus:ring focus:ring-zinc-200 focus:ring-opacity-50 text-sm">
-                                    <option value="all">todos</option>
-                                    @foreach ($roles as $key => $value)
-                                        <option value="{{$key}}">{{$value}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            {{-- busqueda --}}
-                            <div class="flex flex-col items-start w-full mr-1">
-                                <label for="" class="text-sm">Buscar por id de registro</label>
-                                @error('search')
-                                    <span class="text-xs text-red-600">{{ $message }}</span>
-                                @enderror
-                                <input type="text" name="search" placeholder="buscar ..."
-                                    class="p-0.5 w-full rounded-md shadow-sm border-zinc-300 focus:border-zinc-300 focus:ring focus:ring-zinc-200 focus:ring-opacity-50 text-sm">
-                            </div>
-                            {{-- orden de listado --}}
-                            <div class="flex flex-col items-start mr-1">
-                                <label for="" class="text-sm">Orden</label>
-                                <select name="order"
-                                    class="w-36 p-0.5 rounded-md shadow-sm border-zinc-300 focus:border-zinc-300 focus:ring focus:ring-zinc-200 focus:ring-opacity-50 text-sm">
-                                    <option value="asc">a&rarr;z</option>
-                                    <option value="desc">z&rarr;a</option>
-                                </select>
-                            </div>
-                            {{-- submit --}}
-                            <x-buttons.button-submit-green class="ml-1">
-                                <i class="fa-solid fa-magnifying-glass mr-1"></i>
-                                <span>buscar</span>
-                            </x-buttons.button-submit-green>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        {{-- informe de busqueda --}}
-        @if (count($validated) !== 0)
-            <div
-                class="mx-2 p-1 flex items-center justify-between mt-2 text-md text-zinc-700 border border-zinc-300 bg-zinc-200">
-                <div>
-                    <span>filtrado por eventos:
-                        <span class="ml-1 font-bold">
-                            {{ __($validated['event']) }}
-                        </span>
-                        <span class="ml-1">sobre tablas:</span>
-                        <span class="ml-1 font-bold">
-                            @if ($validated['table'] === 'all')
-                                {{__($validated['table'])}}
-                            @else
-                                {{ __(Str::plural(Str::lower(Str::substr($validated['table'], 11)))) }}
-                            @endif
-                        </span>
-                        <span class="ml-1">con responsable:</span>
-                        <span class="ml-1 font-bold">
-                            {{ __($validated['responsible']) }}
-                        </span>
-                        @if (array_key_exists('search', $validated))
-                            @if ($validated['search'] !== null)
-                                <span class="ml-1">con valor de busqueda:</span>
-                                <span class="ml-1 font-bold">
-                                    {{ $validated['search'] }}
-                                </span>
-                            @endif
-                        @endif
-                        <span class="ml-1">ordenado de forma:</span>
-                        <span class="ml-1 font-bold">
-                            {{ __($validated['order']) }}
-                        </span>
-                        <span class="ml-1">por fecha de evento</span>
-                    </span>
-                </div>
-                <div>
-                    {{-- si hay resultados de busqueda o filtro --}}
-                    @if (count($audits) !== 0)
-                        {{-- formulario oculto para enviar parametros de reporte --}}
-                        <form action="{{route('report-audits')}}" method="GET">
-                            <input type="text" name="event" value="{{ $validated['event'] }}" class="hidden">
-                            <input type="text" name="table" value="{{ $validated['table'] }}" class="hidden">
-                            <input type="text" name="responsible" value="{{ $validated['responsible'] }}" class="hidden">
-                            @if (array_key_exists('search', $validated))
-                                <input type="text" name="search" value="{{ $validated['search'] }}" class="hidden">
-                            @endif
-                            <input type="text" name="order" value="{{ $validated['order'] }}" class="hidden">
-                            <button type="submit" title="reporte PDF de la tabla">
-                                <i class="fa-solid fa-file-pdf text-xl text-red-600"></i>
-                            </button>
                         </form>
-                    @endif
+                    </div>
                 </div>
             </div>
-        @endif
+            {{-- informe de busqueda --}}
+            @if (count($validated) !== 0)
+                <div class="m-1 p-1 flex items-center justify-between mt-2 text-md text-zinc-700 border border-zinc-300 bg-zinc-100">
+                    <div>
+                        <span>filtrado por eventos:
+                            <span class="ml-1 font-bold">
+                                {{ __($validated['event']) }}
+                            </span>
+                            <span class="ml-1">sobre tablas:</span>
+                            <span class="ml-1 font-bold">
+                                @if ($validated['table'] === 'all')
+                                    {{__($validated['table'])}}
+                                @else
+                                    {{ __(Str::plural(Str::lower(Str::substr($validated['table'], 11)))) }}
+                                @endif
+                            </span>
+                            <span class="ml-1">con responsable:</span>
+                            <span class="ml-1 font-bold">
+                                {{ __($validated['responsible']) }}
+                            </span>
+                            @if (array_key_exists('search', $validated))
+                                @if ($validated['search'] !== null)
+                                    <span class="ml-1">con valor de busqueda:</span>
+                                    <span class="ml-1 font-bold">
+                                        {{ $validated['search'] }}
+                                    </span>
+                                @endif
+                            @endif
+                            <span class="ml-1">ordenado de forma:</span>
+                            <span class="ml-1 font-bold">
+                                {{ __($validated['order']) }}
+                            </span>
+                            <span class="ml-1">por fecha de evento</span>
+                        </span>
+                    </div>
+                    <div>
+                        {{-- si hay resultados de busqueda o filtro --}}
+                        @if (count($audits) !== 0)
+                            {{-- formulario oculto para enviar parametros de reporte --}}
+                            <form action="{{route('report-audits')}}" method="GET">
+                                <input type="text" name="event" value="{{ $validated['event'] }}" class="hidden">
+                                <input type="text" name="table" value="{{ $validated['table'] }}" class="hidden">
+                                <input type="text" name="responsible" value="{{ $validated['responsible'] }}" class="hidden">
+                                @if (array_key_exists('search', $validated))
+                                    <input type="text" name="search" value="{{ $validated['search'] }}" class="hidden">
+                                @endif
+                                <input type="text" name="order" value="{{ $validated['order'] }}" class="hidden">
+                                <button type="submit" title="reporte PDF de la tabla">
+                                    <i class="fa-solid fa-file-pdf text-xl text-red-600"></i>
+                                </button>
+                            </form>
+                        @endif
+                    </div>
+                </div>
+            @endif
+        </div>
         {{-- tabla --}}
-        <table class="table-auto m-2 border border-zinc-300 border-collapse">
+        <table class="my-2 table-auto border border-zinc-300 border-collapse">
             <thead>
                 <tr>
                     <x-tables.th-cell>id</x-tables.th-cell>
@@ -185,7 +190,7 @@
             @endif
         </table>
         {{-- paginacion --}}
-        <div class="mx-2">
+        <div class="">
             {{ $audits->links() }}
         </div>
     </div>
--- resources/views/audits/show.blade.php
+++ resources/views/audits/show.blade.php
@@ -1,20 +1,27 @@
 @extends('dashboard')
 @section('dashboard-content')
     <div class="w-full h-max flex flex-col p-1">
-        <div class="mx-2 flex flex-col items-start justify-start">
-            <h3 class="text-base text-zinc-800 capitalize">registro de auditoria: cambios</h3>
-            <div class="w-full flex items-center justify-end">
-                <x-buttons.button-link-zinc-light href="{{ route('audits.index') }}" class="mr-1">
-                    <i class="fa-solid fa-rotate-left mr-1"></i>
-                    <span>volver al listado</span>
-                </x-buttons.button-link-zinc-light>
-                <a href="{{route('report-audit', $audit->id)}}" title="reporte PDF de este cambio" class="ml-2">
-                    <i class="fa-solid fa-file-pdf text-xl text-red-600"></i>
-                </a>
+        {{-- encabzado --}}
+        <div class="bg-zinc-300">
+            {{-- titulo de seccion --}}
+            <div class="p-1 flex justify-center items-center bg-zinc-400">
+                <h3 class="text-sm font-bold text-zinc-800 uppercase inline-block">registro de auditoria: cambios</h3>
+            </div>
+            {{-- menu de recargar, filtros, busqueda, reporte --}}
+            <div class="m-1">
+                <div class="w-full flex items-center justify-end">
+                    <a href="{{route('report-audit', $audit->id)}}" title="reporte PDF de este cambio" class="mr-2">
+                        <i class="fa-solid fa-file-pdf text-xl text-red-600"></i>
+                    </a>
+                    <x-buttons.button-link-zinc-light href="{{ route('audits.index') }}">
+                        <i class="fa-solid fa-rotate-left mr-1"></i>
+                        <span>volver al listado</span>
+                    </x-buttons.button-link-zinc-light>
+                </div>
             </div>
         </div>
         {{-- cabecera de informacion general --}}
-        <div class="mx-2 my-2 p-2 mb-4 flex flex-row items-start justify-start border border-zinc-300 border-collapse">
+        <div class="my-1 p-2 mb-4 flex flex-row items-start justify-start border border-zinc-300 border-collapse">
             {{-- informacion de operacion --}}
             <div class="mr-1 p-2">
                 <h4 class="block text-sm uppercase font-semibold tracking-wider text-zinc-600">Informacion de la operación:</h4>
@@ -67,11 +74,11 @@
             </div>
         </div>
         {{-- estado actual del registro a la fecha --}}
-        <div class="mx-2 my-1 flex flex-col items-start justify-start">
+        <div class="mt-1 mb-0.5 flex flex-col items-start justify-start">
             <span class="block text-sm font-semibold tracking-wider text-zinc-600"
                 title="esta primera tabla detalla el estado de cada atributo del registro segun la fecha de operación">Estado del registro a la fecha de cambio:<i class="fa-solid fa-circle-info"></i></span>
         </div>
-        <table class="m-2 border border-zinc-300 border-collapse">
+        <table class="mb-2 mt-0.5 border border-zinc-300 border-collapse">
             @if (count($audit->new_values) !== 0)
                 @foreach ($audit->new_values as $attribute => $value)
                     <tr>
@@ -86,11 +93,11 @@
             @endif
         </table>
         {{-- estado anterior del registro --}}
-        <div class="mx-2 flex flex-col items-start justify-start">
+        <div class="mt-1 mb-0.5 flex flex-col items-start justify-start">
             <span class="block text-sm font-semibold tracking-wider text-zinc-600"
                 title="esta segunda tabla detalla el estado de cada atributo antes de la operación, si existiere">Estado anterior:<i class="fa-solid fa-circle-info"></i></span>
         </div>
-        <table class="m-2 border border-zinc-300 border-collapse">
+        <table class="mb-2 mt-0.5 border border-zinc-300 border-collapse">
             @if (count($audit->old_values) !== 0)
                 @foreach ($audit->old_values as $attribute => $value)
                     <tr>
